a2f83580845d15f80279b31fe9b9d8343d06695e
Jake Ward <jakenicholasward@gmail.com>
2025-03-31 20:51:58 -0400

    implement ParameterComponentModel; add trainig loop

--- apd.py
+++ apd.py
@@ -1,13 +1,117 @@
-# import torch
-# from torch import Tensor
+# %%
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import einops
 
-# class DecomposedModel:
-#     components_CLIJ: Tensor
+    
+class ParameterComponentModel(torch.nn.Module):
+    def __init__(self, n_components: int, input_dim: int, hidden_dim: int, output_dim: int, encoder_bias: nn.Parameter | None = None, decoder_bias: nn.Parameter | None = None):
+        super().__init__()
+        self.n_components = n_components
+        # Initialize encoder weight parameters
+        self.W_enc = torch.nn.Parameter(torch.empty(n_components, input_dim, hidden_dim))
+        # Apply Kaiming initialization
+        for c in range(n_components):
+            nn.init.kaiming_normal_(self.W_enc[c])
+            
+        if encoder_bias is None:
+            self.b_enc = torch.nn.Parameter(torch.zeros(n_components, hidden_dim))   
+        else:
+            self.b_enc = encoder_bias.clone()
+        if decoder_bias is None:
+            self.b_dec = torch.nn.Parameter(torch.zeros(n_components, output_dim))
+        else:
+            self.b_dec = decoder_bias.clone()
+        self.relu = torch.nn.ReLU()
 
-#     def forward()
+    @property
+    def W_dec(self):
+        # Tie decoder weights to transposed encoder weights
+        return torch.transpose(self.W_enc, 1, 2)
+
+    def forward(self, input: torch.Tensor, component_mask: torch.Tensor | None = None):
+        if component_mask is None:
+            component_mask = torch.ones(self.n_components, dtype=torch.long)
+
+        reconstructed_W_enc = einops.einsum(self.W_enc, component_mask, "c i j, c -> i j")
+        reconstructed_b_enc = einops.einsum(self.b_enc, component_mask, "c i, c -> i")
+        # Use the property to get transposed weights
+        W_dec = self.W_dec
+        reconstructed_W_dec = einops.einsum(W_dec, component_mask, "c i j, c -> i j")
+        reconstructed_b_dec = einops.einsum(self.b_dec, component_mask, "c i, c -> i")
+
+        x = input @ reconstructed_W_enc + reconstructed_b_enc
+        return self.relu(x @ reconstructed_W_dec + reconstructed_b_dec)
+
+# %%
+def generate_batch(batch_size: int, n_features: int, feature_probability: float) -> torch.Tensor:
+    """
+    Generates a batch of data of shape (batch_size, n_instances, n_features).
+    """
+    batch_shape = (batch_size, n_features)
+    feat_mag = torch.rand(batch_shape)
+    feat_seeds = torch.rand(batch_shape)
+    return torch.where(feat_seeds <= feature_probability, feat_mag, 0.0)
+# %%
+input = generate_batch(10, 5, 0.2)
+input
+# %%
+from tqdm import tqdm
+# device = "mps"
+
+batch_size = 4096
+n_features = 5
+hidden_dim = 2
+lr = 1e-4
+num_steps = 20000
+sparsity = 0.2
+
+target_model = ParameterComponentModel(1, n_features, hidden_dim, n_features)
+optimizer = torch.optim.Adam(target_model.parameters(), lr=lr)
+critereon = torch.nn.MSELoss()
+
+# get initial loss
+input = generate_batch(batch_size, n_features, sparsity)
+output = target_model(input)
+loss = critereon(output, input)
+print(f"Initial loss: {loss.item()}")
+
+losses = []
+pbar = tqdm(range(num_steps))
+for step in pbar:
+    optimizer.zero_grad()
+    input = generate_batch(batch_size, n_features, sparsity)
+    output = target_model(input)
+    loss = critereon(output, input)
+    loss.backward()
+    optimizer.step()
+    pbar.set_postfix(loss=loss.item())
+    if step % 1000 == 0:
+        losses.append(loss.item())
+
+# %%
+import plotly.express as px
+# loglog plot loss
+fig = px.line(losses, log_x=True, log_y=True)
+fig.show()
+# %%
+test_batch = generate_batch(10, n_features, sparsity)
+test_output = target_model(test_batch)
+print(test_batch)
+print(test_output)
+
+error = (test_batch - test_output)
+px.imshow(error.detach().numpy())
 
 
-def forward(params, input): ...
+# %%
+assert False
+
+# %%
+
+
+
 
 
 def faithfulness(original_params, theta_params):
